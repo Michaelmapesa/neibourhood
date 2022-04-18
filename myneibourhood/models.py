83b91919b56e94f1773e8a7570452545005bf62a
michael mapesa <michaelmapesa93@gmail.com>
2022-04-18 13:32:27 +0300

    added class Profile

--- myneibourhood/models.py
+++ myneibourhood/models.py
@@ -1,4 +1,7 @@
+import profile
 from django.db import models
+from django.dispatch import receiver
+from djano.contrib.auth.models import User
 
 # Create your models here.
 class NeighbourHood(models.Model):
@@ -24,4 +27,27 @@ class NeighbourHood(models.Model):
     @classmethod
     def find_neibourhood(cls, neibourhood_id):
         return cls.objects.filter(id=neibourhood_id)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture=  models.ImageField(upload_to='static/static_dirs/images/')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    NeighbourHood=models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL,null=True, related_name='members',blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender,instance, created):
+        Profile.objects.create(user=instance)
+        if created:
+            Profile.objects.create(user=instance)
+
+            
+    @receiver(post_save, sender=user)
+    def save_user_profile(sender,instance,):
+        instance.profile.save()
         
+
