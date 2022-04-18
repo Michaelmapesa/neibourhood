358ef22dfa2bdc04073ea89508a369f63b064501
michael mapesa <michaelmapesa93@gmail.com>
2022-04-19 02:50:27 +0300

    add hoods view

--- myneibourhood/views.py
+++ myneibourhood/views.py
@@ -1,7 +1,11 @@
+from ast import Param
 import logging
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
+
+from myneibourhood.models import NeighbourHood
 from .forms import SignupForm, BusinessForm
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
@@ -16,9 +20,18 @@ def signup(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
-            loging(request, user)
+            login(request, user)
             return redirect('index')
         else:
             form = SignupForm()
         return render(request,'registration/signup.html',{'form': form})
+
+def hoods(request):
+    all_hoods=NeighbourHood.objects.all()
+    all_hoods=all_hoods[::-1]
+    Params={
+        'all_hoods':all_hoods,
+    }
+    return render(request, 'all_hoods',params)
+    
 
