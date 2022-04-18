a1c38d6a2e9d5f2f6d5071becf0da71b8a7a5f02
michael mapesa <michaelmapesa93@gmail.com>
2022-04-19 02:59:28 +0300

    creating hood members

--- myneibourhood/views.py
+++ myneibourhood/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponseRedirect
 from myneibourhood.models import NeighbourHood
 from .forms import SignupForm, BusinessForm
 from django.contrib.auth import login, authenticate
+from .forms import UpdateProfileForm, NeighbourHoodForm, PostForm
+from .models import NeighbourHood, Profile, Business, Post
 
 
 # Create your views here.
@@ -29,9 +31,53 @@ def signup(request):
 def hoods(request):
     all_hoods=NeighbourHood.objects.all()
     all_hoods=all_hoods[::-1]
-    Params={
+    Params = {
         'all_hoods':all_hoods,
     }
     return render(request, 'all_hoods',params)
+
+def create_hood(request):
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('hood')
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'newhood.html', {'form': form})
+
+def single_hood(request, hood_id):
+    hood = NeighbourHood.objects.get(id=hood_id)
+    business = Business.objects.filter(neighbourhood=hood)
+    posts = Post.objects.filter(hood=hood)
+    posts = posts[::-1]
+    if request.method == 'POST':
+        form = BusinessForm(request.POST)
+        if form.is_valid():
+            biz_form = form.save(commit=False)
+            biz_form.neighbourhood = hood
+            biz_form.user = request.user.profile
+            biz_form.save()
+            return redirect('single-hood', hood.id)
+    else:
+        form = BusinessForm()
+    params = {
+        'hood': hood,
+        'business': business,
+        'form': form,
+        'posts': posts
+    }
+    return render(request, 'single_hood.html', params)
+
+def hood_members(request, hood_id):
+    hood = NeighbourHood.objects.get(id=hood_id)
+    members = Profile.objects.filter(neighbourhood=hood)
+    return render(request, 'members.html', {'members': members})
+
+
+
+
     
 
