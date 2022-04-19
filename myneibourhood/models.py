e3aaf82b62de63ea587d86a679b8b674ccd8eaca
michael mapesa <michaelmapesa93@gmail.com>
2022-04-19 10:18:37 +0300

    change upload location on images

--- myneibourhood/models.py
+++ myneibourhood/models.py
@@ -8,7 +8,7 @@ class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    hood_logo =  models.ImageField(upload_to='static/static_dirs/images/')
+    hood_logo =  models.ImageField(upload_to='neibourhood',null=True)
     description = models.TextField()
     health_info = models.IntegerField(null=True, blank=True)
     health_officer = models.CharField(max_length=60, null=True, blank=True)
@@ -16,7 +16,7 @@ class NeighbourHood(models.Model):
     police_officer = models.CharField(max_length=60, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.name} hood'
+        return f'{self.name} hood1'
 
     def create_neighborhood(self):
         self.save()
@@ -33,7 +33,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=254, blank=True)
-    profile_picture = models.ImageField(upload_to='static/static_dirs/images/')
+    profile_picture = models.ImageField(upload_to='profile',null=True)
     location = models.CharField(max_length=50, blank=True, null=True)
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
