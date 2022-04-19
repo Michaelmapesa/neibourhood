91b8876c483be7a4f7f42f331845b4d5c4115003
michael mapesa <michaelmapesa93@gmail.com>
2022-04-19 12:32:03 +0300

    add image model

--- myneibourhood/models.py
+++ myneibourhood/models.py
@@ -8,7 +8,7 @@ class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    hood_logo =  models.ImageField(upload_to='neibourhood',null=True)
+    hood_logo =  models.ImageField('profile_photos/', default='profile_photos/user')
     description = models.TextField()
     health_info = models.IntegerField(null=True, blank=True)
     health_officer = models.CharField(max_length=60, null=True, blank=True)
@@ -33,7 +33,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=254, blank=True)
-    profile_picture = models.ImageField(upload_to='profile',null=True)
+    profile_picture = models.ImageField('profile_photos/', default='profile_photos/user')
     location = models.CharField(max_length=50, blank=True, null=True)
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
